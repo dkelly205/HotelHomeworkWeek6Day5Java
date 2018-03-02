b80151c07ef37b0020d95f65863071034680d6ca
daniel kelly <danieljkelly91@gmail.com>
2018-03-02 15:55:22 +0000

    Added tests to ensure you cannot add guests to diningRoom or Conference Room is capacity is reached

--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -6,10 +6,14 @@ import static org.junit.Assert.assertEquals;
 public class DiningRoomTest {
 
     DiningRoom diningRoom;
+    DiningRoom diningRoom2;
+    Guest guest1;
 
     @Before
     public void setUp() throws Exception {
         diningRoom = new DiningRoom("Steakhouse", 1, 50);
+        diningRoom2 = new DiningRoom("Italian", 2, 2);
+        guest1 = new Guest("Danny");
     }
 
     @Test
@@ -20,5 +24,24 @@ public class DiningRoomTest {
     @Test
     public void testCapacity() {
         assertEquals(50, diningRoom.getCapacity());
+    }
+
+    @Test
+    public void testNumberOfGuestsStartsEmpty(){
+        assertEquals(0, diningRoom.getNumberOfGuests());
+    }
+
+    @Test
+    public void testCanAddGuestToRoom(){
+        diningRoom.addGuest(guest1);
+        assertEquals(1, diningRoom.getNumberOfGuests());
+    }
+
+    @Test
+    public void testCannotAddGuestToRoomIfCapacityReached(){
+        diningRoom2.addGuest(guest1);
+        diningRoom2.addGuest(guest1);
+        diningRoom2.addGuest(guest1);
+        assertEquals(2, diningRoom2.getNumberOfGuests());
     }
 }
